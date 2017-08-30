87cdc29b68a63bc5d46cb7784f2019005d1a8d32
Anders Martensson <fysikanders@gmail.com>
2017-08-30 23:10:41 +0200

    Implemented functions for cached inverse matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to cache results from computational expensive operations.
+## The cacheSolve function  will calculate the inverse of the matrix the first time it is called. 
+## The result will be cached and any subsequent calls cacheSolve will return the cached value.
+##
+## First create a cachable matrix with makeCacheMatrix and then call cacheSolve
+##
+## Example usage:
+##
+## m <- matrix(c(1,2,3,4,5,6,9,8,9), nrow=3, ncol=3)
+## cachedMatrix <- makeCacheMatrix(m)
+## cachedInverse <- cacheSolve(cachedMatrix)
+##
+## Errors: cacheSolve will stop with an error in case the matrix is not a square matrix. 
 
-## Write a short comment describing this function
 
+## Takes an ordinary function and turns it into a matrix wich will cache the inverse.
+## The function makeCacheMatrix will return a list of functions:
+## makeCacheMatrix$get - Get the underlying matrix 
+## makeCacheMatrix$set - Set new data for the underlying matrix. Any cached inverse will be reset. 
+## makeCacheMatrix$getinverse - Get the cached inverse matrix.
+## makeCacheMatrix$setinverse - Set the cached inverse matrix.
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  
+  set <- function(y) {  # Set new matrix
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
 
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  
+  list(set = set, 
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)  
 }
 
 
-## Write a short comment describing this function
-
+## Calculated the inverse of a cahced matrix and updates the cached value.
+## Input MUST be a cachesMatrix, created by makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(is.null(inv)) {
+    data_matrix <- x$get()
+    dims <- dim(data_matrix)
+    if(dims[1] != dims[2]) {
+      stop('The matrix must be square to be able to find an inverse.')
+    }
+    identitymatrix <- diag(dims[1])      
+    inv <- solve(data_matrix, identitymatrix)
+    x$setinverse(inv)
+  }
+  else {
+    message('Using cached data.')
+  }
+  inv
 }
